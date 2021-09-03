5783d5c92d17752761651047856fe94478e64fde
{StephenPhan95} <{phandinhthan.aof@gmail.com}>
2021-09-03 23:20:01 +0700

    Lession 14 Model in Asp.Net core

---
+++ Webgentle.BookStore/Webgentle.BookStore/Controllers/BookController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Webgentle.BookStore.Models;
+using Webgentle.BookStore.Repository;
+
+namespace Webgentle.BookStore.Controllers
+{
+    public class BookController : Controller
+    {
+        public readonly BookRepository _bookRepository = null;
+        public BookController()
+        {
+            _bookRepository = new BookRepository();
+        }
+        public List<BookModel> GetAllBooks()
+        {
+            return _bookRepository.GetAllBooks();
+        }
+        public BookModel GetBookById(int id)
+        {
+            return _bookRepository.GetBookById(id);
+        }
+        public List<BookModel> SearchBook(string title, string authorName)
+        {
+            return _bookRepository.SearchBook(title, authorName);
+        }
+        
+    }
+}
--- Webgentle.BookStore/Webgentle.BookStore/Startup.cs
+++ Webgentle.BookStore/Webgentle.BookStore/Startup.cs
@@ -7,6 +7,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Server.Kestrel;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Webgentle.BookStore
 {
@@ -16,6 +18,7 @@ namespace Webgentle.BookStore
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -30,10 +33,9 @@ namespace Webgentle.BookStore
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                    endpoints.MapDefaultControllerRoute();
+                }
             });
         }
     }
